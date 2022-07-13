908300666c176744ef907b17702e651667401d53
mailf <mailf.dev@yandex.ru>
2022-07-14 00:20:04 +0300

    add http server

---
+++ api/app.py
@@ -0,0 +1,11 @@
+import spacy
+from fastapi import FastAPI
+
+app = FastAPI()
+emotion_spacy = spacy.load("models/emotion_model")
+
+
+@app.get("/")
+async def check(text: str):
+    result = emotion_spacy(text)
+    return {k: round(v, 3) for k, v in result.cats.items()}
