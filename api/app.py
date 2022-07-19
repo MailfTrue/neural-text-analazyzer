1d394f26ca9f0b9a44744393a05eacb781da6aca
mailf <mailf.dev@yandex.ru>
2022-07-19 16:48:22 +0300

    accept post

--- api/app.py
+++ api/app.py
@@ -6,6 +6,7 @@ emotion_spacy = spacy.load("models/emotion_model")
 
 
 @app.get("/")
+@app.post("/")
 async def check(text: str):
     result = emotion_spacy(text)
     return {k: round(v, 3) for k, v in result.cats.items()}
