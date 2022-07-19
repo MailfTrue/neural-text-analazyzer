6409b9f7459c5c889478a0a77e61e1c197900065
mailf <mailf.dev@yandex.ru>
2022-07-19 17:38:47 +0300

    only post access

--- api/app.py
+++ api/app.py
@@ -1,12 +1,11 @@
 import spacy
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 emotion_spacy = spacy.load("models/emotion_model")
 
 
-@app.get("/")
 @app.post("/")
-async def check(text: str):
+async def check(text: str = Body(...)):
     result = emotion_spacy(text)
     return {k: round(v, 3) for k, v in result.cats.items()}
